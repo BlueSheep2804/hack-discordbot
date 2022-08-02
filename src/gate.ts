62b08d7a023354fdffa7303e5de42861a8d08822
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2022-08-03 00:22:30 +0900

    feat: NODE_ENVの値によってgate_debugを読み込むか否か決める
    NODE_ENVがproductionの場合はデバッグ用のゲート、gate_debugを読み込まないように。

--- src/gate.ts
+++ src/gate.ts
@@ -15,6 +15,7 @@ export class Gate {
     async generateGateList() {
         const gateFiles = fs.readdirSync('./src/embeds').filter(file => file.startsWith('gate_') && (file.endsWith('.js') || file.endsWith('.ts')));
         for (const file of gateFiles) {
+            if (file.split('.')[0] === 'gate_debug' && process.env.NODE_ENV === 'production') continue;
             const gate: GateData = await import(`./embeds/${file.split('.')[0]}`);
             this.gateList[file.split('.')[0]] = gate;
         }
